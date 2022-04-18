9ee77cdbaf86e7364f2e1b9579c5158a332b2ae8
Noah <noahmhodgson@gmail.com>
2022-04-18 19:22:32 -0400

    a little frustrated

--- src/scenes/Load_UFO.ts
+++ src/scenes/Load_UFO.ts
@@ -11,7 +11,7 @@ export class Load_UFO extends BaseGameScene {
     this.game.config.fullscreenTarget
     this.load.image('ufo', 'https://spring-2022-cisc374.github.io/mvp-whatsaveryalleneating/assets/images/player.png');
     this.load.image('background', 'https://spring-2022-cisc374.github.io/mvp-whatsaveryalleneating/assets/images/stomach_background.png');
-    this.load.image('veg', 'https://spring-2022-cisc374.github.io/mvp-whatsaveryalleneating//assets/images/veg.png');
+    this.load.image('veg', 'https://spring-2022-cisc374.github.io/mvp-whatsaveryalleneating/assets/images/veg.png');
   }
 
   public create() {
