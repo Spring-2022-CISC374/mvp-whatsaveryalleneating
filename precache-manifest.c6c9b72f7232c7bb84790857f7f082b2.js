84a3ee4c6f31868a79a887ec2710827748a716a4
Noah <noahmhodgson@gmail.com>
2022-05-04 12:39:21 -0400

    Revert "modified digestive system"
    This reverts commit 14074f1ce6eae4cc61b499132b45a24f9b221146.

---
+++ precache-manifest.c6c9b72f7232c7bb84790857f7f082b2.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ea102634d0e26cfa7804a4859203fb",
+    "url": "assets/alien.png"
+  },
+  {
+    "revision": "8adedfd6ccefda3318d9dd430a9c1bdd",
+    "url": "assets/background.mp3"
+  },
+  {
+    "revision": "4b7cb5836d69dd7e2f7e36c33bca4eb4",
+    "url": "assets/block-32-A.png"
+  },
+  {
+    "revision": "f490d48644ec4a61dcbb5600f2cb11df",
+    "url": "assets/block-32-B.png"
+  },
+  {
+    "revision": "75a076cd483897868a5b6a79239586d3",
+    "url": "assets/block-32-C.png"
+  },
+  {
+    "revision": "c8a5c1005360c7a7e3eeb1eea73efc62",
+    "url": "assets/block-32-Cal.png"
+  },
+  {
+    "revision": "100a30b3ae9b1f106f88550d2e08a902",
+    "url": "assets/block-32-D.png"
+  },
+  {
+    "revision": "7d32b03145d720d34a8e45e9f286a75d",
+    "url": "assets/block-32-K.png"
+  },
+  {
+    "revision": "864f0453c6efea79bcf614a130dcb451",
+    "url": "assets/block-32.png"
+  },
+  {
+    "revision": "b995fe922e1bb0df3209b97d80d993d0",
+    "url": "assets/block-64-A .png"
+  },
+  {
+    "revision": "c85ddb79c4a441baf0ade02c314e8190",
+    "url": "assets/block-64-B.png"
+  },
+  {
+    "revision": "8834e1d15a42015def0e611391d888bc",
+    "url": "assets/block-64-C.png"
+  },
+  {
+    "revision": "0e5c6bfd61838c4b1b980d8705d6abf5",
+    "url": "assets/block-64-Cal.png"
+  },
+  {
+    "revision": "fcf10f3c9cda04d22907184ee328fac9",
+    "url": "assets/block-64-D.png"
+  },
+  {
+    "revision": "9889be83217dea3fedf148536d9b4663",
+    "url": "assets/block-64-K .png"
+  },
+  {
+    "revision": "dc3b22a8a1e59b032747eb178d556f58",
+    "url": "assets/block-64.png"
+  },
+  {
+    "revision": "614fec9801d664ed68dc0feba0d0cb41",
+    "url": "assets/click.wav"
+  },
+  {
+    "revision": "4c054041144666bcdaeae1387e3148f3",
+    "url": "assets/fonts/.DS_Store"
+  },
+  {
+    "revision": "ae0d51b037c520f6280e5fe59b7f594c",
+    "url": "assets/fonts/minecraftia/minecraftia.png"
+  },
+  {
+    "revision": "8b59d4d5bf1973dbc9201a977392649a",
+    "url": "assets/fonts/minecraftia/minecraftia.xml"
+  },
+  {
+    "revision": "194577a7e20bdcc7afbb718f502c134c",
+    "url": "assets/images/.DS_Store"
+  },
+  {
+    "revision": "54e1383f981c650382ea2a271f624288",
+    "url": "assets/images/background1.png"
+  },
+  {
+    "revision": "6b2a85025ccd56b827a906a42000f278",
+    "url": "assets/images/beef.png"
+  },
+  {
+    "revision": "19f7e0ddaf86e4d4e02bbc2760e2d94c",
+    "url": "assets/images/candy.png"
+  },
+  {
+    "revision": "3d527ef5f2108b94f8509d9466737cb0",
+    "url": "assets/images/foreground1.png"
+  },
+  {
+    "revision": "7086c43f35233b7cc7e6a16403da846d",
+    "url": "assets/images/fries.png"
+  },
+  {
+    "revision": "e9955b182283d8569298e442fa25967c",
+    "url": "assets/images/logo.jpeg"
+  },
+  {
+    "revision": "ca47a176868ce2faaeaf2c0ec2615ae0",
+    "url": "assets/images/milk.png"
+  },
+  {
+    "revision": "22d6058793952198a2591862633e5df6",
+    "url": "assets/images/minus1.png"
+  },
+  {
+    "revision": "e5ab3c3ea7e060f10e1cc97716179c50",
+    "url": "assets/images/player.png"
+  },
+  {
+    "revision": "69c3376ad450b1e1be86b76e21b5bfb9",
+    "url": "assets/images/plus1.png"
+  },
+  {
+    "revision": "b73b474d0c29d1c4001e44de7b618a31",
+    "url": "assets/images/qqq.png"
+  },
+  {
+    "revision": "377d3d458729a41e57f9552d12522f8f",
+    "url": "assets/images/stomach_background.png"
+  },
+  {
+    "revision": "c423a7bf00221346165ef1bd84f114ed",
+    "url": "assets/images/tree.png"
+  },
+  {
+    "revision": "7e4bdad2aa563285c7703996b46bb34b",
+    "url": "assets/images/veg.png"
+  },
+  {
+    "revision": "30ac59f49732441a263c800806effd32",
+    "url": "assets/lettuce.png"
+  },
+  {
+    "revision": "666f45bd16825db0febb374268768d5a",
+    "url": "assets/lineBreak.wav"
+  },
+  {
+    "revision": "d6846d0efbffc88809e312ff260d0421",
+    "url": "assets/peanut.png"
+  },
+  {
+    "revision": "4770fd8ca8e3a3362e3d5173f8bb0b65",
+    "url": "assets/play-64.png"
+  },
+  {
+    "revision": "6a35959dca9009e4da01f88bca3a68ae",
+    "url": "assets/steak.png"
+  },
+  {
+    "revision": "8f8a8b98c655870f6b0502069cc630bc",
+    "url": "assets/tickSound.wav"
+  },
+  {
+    "revision": "1eb69a1e3e9e7b596a256b37ea87c04b",
+    "url": "index.html"
+  },
+  {
+    "revision": "77afb22ad8e4b1ea5d2b",
+    "url": "main.b6e2abb29fcc39dfe970.bundle.js"
+  },
+  {
+    "revision": "d1653d20b43f3e9b89f28fbe575cdcf7",
+    "url": "main.bundle.js"
+  },
+  {
+    "revision": "892f371075c5cc9b09ade971d2d5ae75",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "sw.js"
+  },
+  {
+    "revision": "fd542abf3c2354192eb7",
+    "url": "vendors.4995edf3fcab7c38681c.bundle.js"
+  },
+  {
+    "revision": "b12bfa65be53ffc08a7e535331ddd86c",
+    "url": "vendors.bundle.js"
+  },
+  {
+    "revision": "5141139a7368122610875a5fa9ebec1a",
+    "url": "webpack.config.js"
+  }
+]);
